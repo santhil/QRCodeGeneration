fd651adecbd96ff33552a2ae4901b9b1c6f06b23
Senthil.Gunasekaran <Senthil.Gunasekaran@KANINI-LTP-877>
2022-12-26 12:36:38 +0530

    Refactored Program.cs configurations

--- QRCodeGeneration/Program.cs
+++ QRCodeGeneration/Program.cs
@@ -1,32 +1,41 @@
-using QRCodeGeneration.Data;
-//using QRCodeGeneration.Repositories;
+﻿using QRCodeGeneration.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-////--------------------------------------------------------------------
-//builder.Services.AddScoped<IQRCodeService, QRCodeService>();
-builder.Services.AddDbContext<DbContextClass>();
-//--------------------------------------------------------------------
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddApplicationInsightsTelemetry();
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace QRCodeGeneration
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var buildedApp = BuildQRCodeGenerationApp(args);
+            var application = ConfigureQRCodeGenerationApp(buildedApp);
+            application.Run();
+        }
+
+        public static WebApplication BuildQRCodeGenerationApp(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var services = builder.Services;
+            services.AddDbContext<DbContextClass>();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddApplicationInsightsTelemetry();
+            return builder.Build();
+               
+        }
+
+        private static WebApplication ConfigureQRCodeGenerationApp(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            return app;
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
