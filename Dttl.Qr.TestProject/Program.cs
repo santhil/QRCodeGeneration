2ed101448af40e13a1c0311d5d2208e8fbb47c4d
Senthil.Gunasekaran <Senthil.Gunasekaran@KANINI-LTP-877>
2022-12-27 18:44:03 +0530

    Program file optimisation

--- Dttl.Qr.TestProject/Program.cs
+++ Dttl.Qr.TestProject/Program.cs
@@ -4,15 +4,17 @@ using Dttl.Qr.Util;
 
 string fileName = "ProfilePhoto.jpg";
 
-VCardQRCode c = new VCardQRCode();
-c.FirstName = "Praveen";
-c.LastName = "Ranjan";
-c.CompanyName = "Kanini";
-c.Designation = "Software Developer";
-c.MobileNo = "9000000000";
-c.EmailId = "Test@kanini.com";
-c.Website = "kanini.com";
-c.PersonalLinks = "www.google.com";
-c.UploadImage = await System.IO.File.ReadAllBytesAsync(fileName);
+VCardQRCode c = new()
+{
+    FirstName = "Praveen",
+    LastName = "Ranjan",
+    CompanyName = "Kanini",
+    Designation = "Software Developer",
+    MobileNo = "9000000000",
+    EmailId = "Test@kanini.com",
+    Website = "kanini.com",
+    PersonalLinks = "www.google.com",
+    UploadImage = await System.IO.File.ReadAllBytesAsync(fileName)
+};
 
 File.WriteAllText("Vcard.vcf", VCardUtil.GetVCard(c));
