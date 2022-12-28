3b807b8818abc05a77ecc90dfd4b42ffcba0940c
Senthil.Gunasekaran <Senthil.Gunasekaran@KANINI-LTP-877>
2022-12-28 13:35:29 +0530

    Test Project Added

---
+++ Dttl.Qr.ServiceTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dttl.Qr.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dttl.Qr.Service.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
